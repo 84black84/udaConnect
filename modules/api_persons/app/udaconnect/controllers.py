98ca60c34e20d090f58ce7c6d1d7ea09315b4ada
Christos Mavrogiannis <christos.mavrogianns@gmail.com>
2022-11-25 12:50:41 +0100

    Finalizing person api swagger documentation

--- modules/api_persons/app/udaconnect/controllers.py
+++ modules/api_persons/app/udaconnect/controllers.py
@@ -1,43 +1,51 @@
 from app.udaconnect.models import Person
 from app.udaconnect.schemas import PersonSchema
 from app.udaconnect.services import PersonService
-from flask import request
+from flask import request, abort
 from flask_accepts import accepts, responds
-from flask_restx import Namespace, Resource, fields
+from flask_restx import Namespace, Resource
 from typing import List
 
 api = Namespace("UdaConnect", description="Connections via geolocation - Persons API.")
-person_fields = api.model('Person', {
-    'id': fields.Integer,
-    'first_name': fields.String,
-    'last_name': fields.String,
-    'company_name': fields.String
-    })
+
+person_fields = Person.get_person_fields(api)
 
 @api.route("/persons", endpoint='persons')
 class PersonsResource(Resource):
     @accepts(schema=PersonSchema)
     @responds(schema=PersonSchema)
-    @api.doc(responses={201: 'Returns the newly created person object'},  body=person_fields)
+    @api.doc(description="Create a new person", responses={ 201: 'Returns the newly created person object', 400: 'Was not able to create a new person' },  body=person_fields)
     def post(self) -> Person:
-        payload = request.get_json()
-        new_person: Person = PersonService.create(payload)
-        return new_person, 201
+        new_person = Person
+        try:
+            payload = request.get_json()
+            new_person: Person = PersonService.create(payload)
+            # raise ValueError('error reised for testing purposes.')
+            return new_person, 201
+        except Exception as e:
+            print('Failed to create a new person: ' + str(e))
+            abort(400, "Failed to create a new person")
 
     @responds(schema=PersonSchema, many=True)
-    @api.doc(responses={200: 'Returns a list of all the available person objects'})
+    @api.doc(description = "Return all the person objects", responses={200: 'Returns a list of all the available person objects', 400: 'Was not able to retrieve the person objects'})
     @api.marshal_with(person_fields, as_list=True)
     def get(self) -> List[Person]:
-        persons: List[Person] = PersonService.retrieve_all()
-        return persons
+        try: 
+            persons: List[Person] = PersonService.retrieve_all()
+            return persons
+        except Exception as e:
+            print('Failed to retrieve the person objects. Details: ' + str(e))
+            abort(400, "Failed to retrieve the person objects")
+        
 
 @api.route("/persons/<person_id>")
 @api.param("person_id", "Unique ID for a given Person", _in="query")
-@api.doc("Gets the id of a person as paramter and returns the relater person object")
 class PersonResource(Resource):
     @responds(schema=PersonSchema)
-    @api.doc(responses= { 200: 'Returns a person object based on the given person_id' })
+    @api.doc(description = "Gets the id of a person as parameter and returns the relater person object", responses= { 200: 'Returns a person object based on the given person_id', 404: 'Person not found' })
     @api.marshal_with(person_fields)
     def get(self, person_id) -> Person:
         person: Person = PersonService.retrieve(person_id)
+        if person is None:
+            abort(404, "Person not found")
         return person
--- modules/api_persons/app/udaconnect/models.py
+++ modules/api_persons/app/udaconnect/models.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
-from app import db  # noqa
+from app import db
 from sqlalchemy import Column, Integer, String
+from flask_restx import fields
 
 class Person(db.Model):
     __tablename__ = "person"
@@ -9,3 +10,12 @@ class Person(db.Model):
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
     company_name = Column(String, nullable=False)
+    
+    @staticmethod
+    def get_person_fields(api):
+        return api.model('Person', {
+            'id': fields.Integer(description='person id', example=1),
+            'first_name': fields.String(description="person's first name", example="Tom"),
+            'last_name': fields.String(description="person's last name", example="Hood"),
+            'company_name': fields.String(description="person's company name", example="The Hood company")
+            })
